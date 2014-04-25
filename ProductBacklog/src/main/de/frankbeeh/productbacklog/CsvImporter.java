8b981cebf97472ab42bdbf5047b4254cc1d43eef
fbe <fbe@ZRHN1477.ads.zuehlke.com>
2014-04-25 20:25:29 +0200

    [1] Commit project setup.

---
+++ ProductBacklog/src/main/de/frankbeeh/productbacklog/CsvImporter.java
@@ -0,0 +1,5 @@
+package de.frankbeeh.productbacklog;
+
+public class CsvImporter {
+
+}
---
+++ ProductBacklog/src/test/de/frankbeeh/productbacklog/CsvImporterTest.java
@@ -0,0 +1,14 @@
+package de.frankbeeh.productbacklog;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CsvImporterTest {
+
+	@Test
+	public void test() {
+		new CsvImporter();
+	}
+
+}
