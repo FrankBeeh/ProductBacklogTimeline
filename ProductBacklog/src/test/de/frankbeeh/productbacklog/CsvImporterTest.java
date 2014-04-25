5caa24c8253476d85633bb0f6bb99c9f83045f37
fbe <fbe@ZRHN1477.ads.zuehlke.com>
2014-04-25 20:45:26 +0200

    [1] Cleanup and formatting in save actions.

--- ProductBacklog/src/test/de/frankbeeh/productbacklog/CsvImporterTest.java
+++ ProductBacklog/src/test/de/frankbeeh/productbacklog/CsvImporterTest.java
@@ -1,14 +1,12 @@
 package de.frankbeeh.productbacklog;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class CsvImporterTest {
 
-	@Test
-	public void test() {
-		new CsvImporter();
-	}
+  @Test
+  public void test() {
+    new CsvImporter();
+  }
 
 }
