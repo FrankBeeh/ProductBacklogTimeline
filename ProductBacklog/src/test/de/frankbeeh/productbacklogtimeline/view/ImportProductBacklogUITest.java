cd8ed14e33fc47d40f91b040be8284f676aceeaa
fbe <fbe@ZRHN1477.ads.zuehlke.com>
2014-05-01 13:49:03 +0200

    UI-Test: Open a PBL file and show the content of the PBL tab.

--- ProductBacklog/src/test/de/frankbeeh/productbacklogtimeline/view/ImportProductBacklogUITest.java
+++ ProductBacklog/src/test/de/frankbeeh/productbacklogtimeline/view/ImportProductBacklogUITest.java
@@ -14,48 +14,64 @@ import org.loadui.testfx.GuiTest;
 
 public class ImportProductBacklogUITest extends GuiTest {
 
-	private static final String APPLICATION_FXML = "main.fxml";
+    private static final String APPLICATION_FXML = "main.fxml";
 
-	@Override
-	protected Parent getRootNode() {
-		final FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource(APPLICATION_FXML));
-		try {
-			fxmlLoader.load();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return fxmlLoader.getRoot();
-	}
+    @Override
+    protected Parent getRootNode() {
+        final FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource(APPLICATION_FXML));
+        try {
+            fxmlLoader.load();
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+        return fxmlLoader.getRoot();
+    }
 
-	@Test
-	public void importBacklog() throws Exception {
-		importBacklogFile();
-		closeDialog();
-		selectSprintTab();
-	}
+    @Test
+    public void importBacklog() throws Exception {
+        importBacklogFile();
+        enterFileName();
+        selectPBLTab();
+    }
 
-	private void selectSprintTab() {
-		click("Sprints");
-		assertEquals("Sprints", getSelectedTabTitle());
-	}
+    @Test
+    public void cancelImportBacklog() throws Exception {
+        importBacklogFile();
+        closeDialog();
+        selectPBLTab();
+    }
 
-	private String getSelectedTabTitle() {
-		return this.<TabPane> getNode("#mainTabPane").getTabs().get(1).getText();
-	}
+    private void enterFileName() {
+        type("PBL11.csv");
+        type(KeyCode.ENTER);
+    }
 
-	private void importBacklogFile() {
-		click("File");
-		click("Import PBL");
-	}
+    private void selectPBLTab() {
+        click("PBL");
+        assertEquals("PBL", getSelectedTabTitle());
+    }
 
-	private void closeDialog() {
-		press(KeyCode.ALT);
-		press(KeyCode.F4);
-		release(KeyCode.ALT);
-	}
+    private String getSelectedTabTitle() {
+        return getTabPane().getSelectionModel().getSelectedItem().getText();
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T> T getNode(final String theSelector) {
-		return (T) find(theSelector);
-	}
+    private TabPane getTabPane() {
+        return this.<TabPane> getNode("#mainTabPane");
+    }
+
+    private void importBacklogFile() {
+        click("File");
+        click("Import PBL");
+    }
+
+    private void closeDialog() {
+        press(KeyCode.ALT);
+        press(KeyCode.F4);
+        release(KeyCode.ALT);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getNode(final String theSelector) {
+        return (T) find(theSelector);
+    }
 }
