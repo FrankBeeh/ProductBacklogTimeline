8693c26290fca73aa446998e50b6a4029ab1bca3
Frank Beeh <FrankBeeh@web.de>
2015-04-19 01:37:27 +0200

    Revert "Show the difference of the accumulated estimate."
    This reverts commit 531b0cedb4a3aedba378341b44d7a53ba0889e01.

--- ProductBacklog/src/main/de/frankbeeh/productbacklogtimeline/data/ProductBacklogComparisonItem.java
+++ ProductBacklog/src/main/de/frankbeeh/productbacklogtimeline/data/ProductBacklogComparisonItem.java
@@ -1,11 +1,7 @@
 package de.frankbeeh.productbacklogtimeline.data;
 
-import java.text.DecimalFormat;
-
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-
-import com.google.common.base.Strings;
 
 public class ProductBacklogComparisonItem {
     private final ProductBacklogItem productBacklogItem;
@@ -14,6 +10,19 @@ public class ProductBacklogComparisonItem {
     public ProductBacklogComparisonItem(ProductBacklogItem productBacklogItem, ProductBacklogItem referenceProductBacklogItem) {
         this.productBacklogItem = productBacklogItem;
         this.referenceProductBacklogItem = referenceProductBacklogItem;
+    }
+
+    public String getCompletionForecastDescription(String progressForecastName) {
+        final SprintData sprintData = productBacklogItem.getCompletionForecast(progressForecastName);
+        if (sprintData == null) {
+            return null;
+        } else {
+            SprintData referenceCompletionForecast = null;
+            if (referenceProductBacklogItem != null) {
+                referenceCompletionForecast = referenceProductBacklogItem.getCompletionForecast(progressForecastName);
+            }
+            return sprintData.getDescription(referenceCompletionForecast);
+        }
     }
 
     public String getId() {
@@ -40,6 +49,14 @@ public class ProductBacklogComparisonItem {
         return productBacklogItem.getAccumulatedEstimate();
     }
 
+    public String getAccumulatedEstimateDescription() {
+        final Double accumulatedEstimate = getAccumulatedEstimate();
+        if (accumulatedEstimate == null) {
+            return null;
+        }
+        return accumulatedEstimate.toString();
+    }
+
     public Double getEstimate() {
         return productBacklogItem.getEstimate();
     }
@@ -50,44 +67,6 @@ public class ProductBacklogComparisonItem {
 
     public Integer getRank() {
         return productBacklogItem.getRank();
-    }
-
-    public String getCompletionForecastDescription(String progressForecastName) {
-        final SprintData sprintData = productBacklogItem.getCompletionForecast(progressForecastName);
-        if (sprintData == null) {
-            return null;
-        } else {
-            SprintData referenceCompletionForecast = null;
-            if (referenceProductBacklogItem != null) {
-                referenceCompletionForecast = referenceProductBacklogItem.getCompletionForecast(progressForecastName);
-            }
-            return sprintData.getDescription(referenceCompletionForecast);
-        }
-    }
-
-    public String getAccumulatedEstimateDescription() {
-        final StringBuilder stringBuilder = new StringBuilder();
-        final Double accumulatedEstimate = getAccumulatedEstimate();
-        if (accumulatedEstimate == null) {
-            return null;
-        }
-        final String formattedEstimate = accumulatedEstimate.toString();
-        stringBuilder.append(formattedEstimate);
-        if (referenceProductBacklogItem != null && referenceProductBacklogItem.getAccumulatedEstimate() != null) {
-            final double accumulatedEstimateDifference = accumulatedEstimate - referenceProductBacklogItem.getAccumulatedEstimate();
-            if (accumulatedEstimateDifference != 0) {
-                final String formattedDifference = "(" + new DecimalFormat("+0.0;-0.0").format(accumulatedEstimateDifference) + ")";
-                int count = formattedDifference.length() - formattedEstimate.length() + 1;
-                if (count > 0) {
-                    stringBuilder.insert(0, Strings.repeat(" ", count));
-                    stringBuilder.append("\n");
-                } else {
-                    stringBuilder.append("\n").append(Strings.repeat(" ", 3 - count));
-                }
-                stringBuilder.append(formattedDifference);
-            }
-        }
-        return stringBuilder.toString();
     }
 
     @Override
--- ProductBacklog/src/test/de/frankbeeh/productbacklogtimeline/view/ImportProductBacklogUITest.java
+++ ProductBacklog/src/test/de/frankbeeh/productbacklogtimeline/view/ImportProductBacklogUITest.java
@@ -16,6 +16,7 @@ public class ImportProductBacklogUITest extends BaseUITest {
     private static final String SPRINT_5 = "Sprint 5\n04.05.2014";
     private static final String SPRINT_6 = "Sprint 6\n11.05.2014";
     private static final String SPRINT_7 = "Sprint 7\n18.05.2014";
+    private static final String SPRINT_8 = "Sprint 8\n25.05.2014";
     private static final String PBL_FILE_1 = "PBL1.csv";
     private static final String PBL_FILE_2 = "PBL2.csv";
     private static final String VELOCITY_FORECAST_FILE_1 = "VelocityForecast1.csv";
@@ -33,19 +34,19 @@ public class ImportProductBacklogUITest extends BaseUITest {
     private static final TableViewContent PBL_1_COMPARED_TO_PBL_2 = new TableViewContent(new String[][] {
             { "1", "3.0", "Done", "PBI 1", "Description 1", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
             { "2", "1.0", "Canceled", "PBI 2", "Description 2", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
-            { "3", "5.0", "Todo", "PBI 3", "Description 3", "     8.0\n(-13.0)", "", "Sprint 3\n(Sprint 4)\n27.04.2014\n(-3d)", "Sprint 3\n(Sprint 4)\n27.04.2014\n(-3d)", SPRINT_3 },
-            { "4", "8.0", "Todo", "PBI 4", "Description 4", "    16.0\n(-10.0)", "", SPRINT_5, SPRINT_5, "Sprint 5\n(Sprint 4)\n04.05.2014\n(+4d)" } });
+            { "3", "5.0", "Todo", "PBI 3", "Description 3", "8.0", "", "Sprint 3\n(Sprint 4)\n27.04.2014\n(-3d)", "Sprint 3\n(Sprint 4)\n27.04.2014\n(-3d)", SPRINT_3 },
+            { "4", "8.0", "Todo", "PBI 4", "Description 4", "16.0", "", "Sprint 5\n(Sprint 6)\n04.05.2014\n(-7d)", SPRINT_5, SPRINT_5 } });
 
     private static final TableViewContent PBL_2_WITH_FORECAST_1 = new TableViewContent(new String[][] {
             { "1", "3.0", "Done", "PBI 1", "Description 1", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
             { "2", "1.0", "Canceled", "PBI 2", "Description 2", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
             { "5", "13.0", "Done", "PBI 5", "Description 5", "16.0", "Sprint 2", SPRINT_5, SPRINT_5, SPRINT_5 },
-            { "3", "5.0", "Todo", "PBI 3", "Description 3", "21.0", "", SPRINT_6, SPRINT_6, SPRINT_6 }, { "4", "5.0", "Todo", "PBI 4", "Description 4", "26.0", "", SPRINT_7, SPRINT_7, SPRINT_7 },
-            { "6", "2.0", "Todo", "PBI 6", "Description 6", "28.0", "", SPRINT_7, SPRINT_7, SPRINT_7 } });
+            { "3", "5.0", "Todo", "PBI 3", "Description 3", "21.0", "", SPRINT_6, SPRINT_6, SPRINT_6 }, { "4", "8.0", "Todo", "PBI 4", "Description 4", "29.0", "", SPRINT_7, SPRINT_7, SPRINT_7 },
+            { "6", "2.0", "Todo", "PBI 6", "Description 6", "31.0", "", SPRINT_8, SPRINT_8, SPRINT_8 } });
 
     private static final TableViewContent PBL_2_WITH_FORECAST_2 = new TableViewContent(new String[][] {
             // FIXME: The PBIs are not ordered in the content, but in the UI it's ok.
-            { "4", "5.0", "Todo", "PBI 4", "Description 4", "26.0", "", SPRINT_5, SPRINT_5, SPRINT_4 }, { "6", "2.0", "Todo", "PBI 6", "Description 6", "28.0", "", SPRINT_6, SPRINT_5, SPRINT_5 },
+            { "4", "8.0", "Todo", "PBI 4", "Description 4", "29.0", "", SPRINT_6, SPRINT_5, SPRINT_5 }, { "6", "2.0", "Todo", "PBI 6", "Description 6", "31.0", "", SPRINT_6, SPRINT_5, SPRINT_5 },
             { "1", "3.0", "Done", "PBI 1", "Description 1", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
             { "2", "1.0", "Canceled", "PBI 2", "Description 2", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
             { "5", "13.0", "Done", "PBI 5", "Description 5", "16.0", "Sprint 2", SPRINT_2, SPRINT_2, SPRINT_2 },
@@ -55,9 +56,9 @@ public class ImportProductBacklogUITest extends BaseUITest {
             { "1", "3.0", "Done", "PBI 1", "Description 1", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
             { "2", "1.0", "Canceled", "PBI 2", "Description 2", "3.0", "Sprint 1", SPRINT_1, SPRINT_1, SPRINT_1 },
             { "5", "13.0", "Done", "PBI 5", "Description 5", "16.0", "Sprint 2", SPRINT_2, SPRINT_2, SPRINT_2 },
-            { "3", "5.0", "Todo", "PBI 3", "Description 3", "    21.0\n(+13.0)", "", "Sprint 4\n(Sprint 3)\n30.04.2014\n(+3d)", "Sprint 4\n(Sprint 3)\n30.04.2014\n(+3d)", SPRINT_3 },
-            { "4", "5.0", "Todo", "PBI 4", "Description 4", "    26.0\n(+10.0)", "", SPRINT_5, SPRINT_5, "Sprint 4\n(Sprint 5)\n30.04.2014\n(-4d)" },
-            { "6", "2.0", "Todo", "PBI 6", "Description 6", "28.0", "", SPRINT_6, SPRINT_5, SPRINT_5 } });
+            { "3", "5.0", "Todo", "PBI 3", "Description 3", "21.0", "", "Sprint 4\n(Sprint 3)\n30.04.2014\n(+3d)", "Sprint 4\n(Sprint 3)\n30.04.2014\n(+3d)", SPRINT_3 },
+            { "4", "8.0", "Todo", "PBI 4", "Description 4", "29.0", "", "Sprint 6\n(Sprint 5)\n11.05.2014\n(+7d)", SPRINT_5, SPRINT_5 },
+            { "6", "2.0", "Todo", "PBI 6", "Description 6", "31.0", "", SPRINT_6, SPRINT_5, SPRINT_5 } });
 
     private static final TableViewContent VELOCITY_FORECAST_1 = new TableViewContent(new String[][] { { "Sprint 1", "25.04.2014", "25.04.2014", "4.0", "4.0", "6.0", "3.0", "3.0", "3.0", "3.0", "", "", "", "" },
             { "Sprint 2", "26.04.2014", "26.04.2014", "8.0", "", "", "", "4.0", "4.0", "4.0", "", "7.0", "7.0", "7.0" },
