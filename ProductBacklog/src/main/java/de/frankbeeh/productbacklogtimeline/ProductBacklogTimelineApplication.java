99ccebe1318e3a3206b59e2e55a40cc33c567b30
Frank Beeh <FrankBeeh@web.de>
2015-04-20 21:06:41 +0200

    Show the version of the application read from the manifest. Correct the name of the css file.

--- ProductBacklog/src/main/java/de/frankbeeh/productbacklogtimeline/ProductBacklogTimelineApplication.java
+++ ProductBacklog/src/main/java/de/frankbeeh/productbacklogtimeline/ProductBacklogTimelineApplication.java
@@ -1,5 +1,11 @@
 package de.frankbeeh.productbacklogtimeline;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,6 +17,8 @@ public class ProductBacklogTimelineApplication extends Application {
 
     private static final String APPLICATION_TITLE = "Product Backlog Timeline";
     private static final String APPLICATION_FXML = "view/main.fxml";
+    private static final String MANIFEST_FILE = "/META-INF/MANIFEST.MF";
+    private static final String CSS_FILE = "css/productbacklog.css";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -18,13 +26,26 @@ public class ProductBacklogTimelineApplication extends Application {
         final Parent root = (Parent) loader.load();
 
         final Scene scene = new Scene(root, 1200, 800);
-        scene.getStylesheets().add(ProductBacklogTimelineApplication.class.getResource("css/productBacklog.css").toExternalForm());
-        primaryStage.setTitle(APPLICATION_TITLE);
+        scene.getStylesheets().add(getClass().getResource(CSS_FILE).toExternalForm());
+        primaryStage.setTitle(APPLICATION_TITLE + " - " + readVersionFromManifest());
         primaryStage.setScene(scene);
         primaryStage.show();
 
         final MainController controller = loader.getController();
         controller.initController(primaryStage);
+    }
+
+    private String readVersionFromManifest() throws MalformedURLException, IOException {
+        final Class<ProductBacklogTimelineApplication> clazz = ProductBacklogTimelineApplication.class;
+        final String className = clazz.getSimpleName() + ".class";
+        final String classPath = clazz.getResource(className).toString();
+        if (!classPath.startsWith("jar")) {
+            return "0.0";
+        }
+        final String manifestPath = classPath.substring(0, classPath.lastIndexOf("!") + 1) + MANIFEST_FILE;
+        final Manifest manifest = new Manifest(new URL(manifestPath).openStream());
+        final Attributes attr = manifest.getMainAttributes();
+        return attr.getValue("Implementation-Version");
     }
 
     public static void main(String[] args) {
